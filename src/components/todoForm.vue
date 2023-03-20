33501c7c3b84c222e07a7eff8547dd213166d8f5
Yared <yared241993@gmail.com>
2023-03-20 03:29:34 -0700

    updated button background

--- src/components/todoForm.vue
+++ src/components/todoForm.vue
@@ -48,7 +48,7 @@
         <div class="md:w-1/3"></div>
         <div class="md:w-2/3">
           <button
-            class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            class="shadow bg-red-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
             type="button"
           >
             Sign Up
