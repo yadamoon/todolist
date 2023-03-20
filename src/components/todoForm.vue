3355eff06e95db8668e18999870b2c5d26b13a67
Yared <yared241993@gmail.com>
2023-03-20 03:24:04 -0700

    updated the todo form component

--- src/components/todoForm.vue
+++ src/components/todoForm.vue
@@ -7,7 +7,7 @@
             class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
             for="inline-full-name"
           >
-            Full Name
+            Full name
           </label>
         </div>
         <div class="md:w-2/3">
